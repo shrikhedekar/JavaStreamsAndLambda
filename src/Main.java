7b73ccc85c9365578e5571cdd64ba4b4b9a19275
shrikhedekar <shri.khedekar@gmail.com>
2025-07-22 18:33:48 +0530

    Method reference examples added

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to the demo on Streams and Lambda!");
         var names =  List.of("Susan","John","Michel", "Mitchell");
-        /*for (String name : names) {
+
+        // Traversing list conventional way
+        for (String name : names) {
             System.out.println("Names using for : " + name);
         }
 
-        names.forEach(new Consumer<String>() {
+        // Traversing list using anonyms class to define the required functional interface
+        names.forEach(new Consumer<>() {
             @Override
             public void accept(String name) {
                 System.out.println("Names using Anonymous class : "+name);
             }
         });
 
+        // Using a lambda with reduced code following its syntax
         names.forEach(name ->System.out.println("Names using Lambda : "+name));
 
         // Defining lambda
         Consumer <String> print = name -> System.out.println("Names using Lambda : "+name);
-
-        names.forEach(print);*/
+        names.forEach(print);
 
 
         // using local variables in lambda expressions
-
         var message = "Hello ";
-
         names.forEach(name ->System.out.println(message+name));
 
 
--- src/PrintNames.java
+++ src/PrintNames.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+// This class demos the different types of method references
 
 public class PrintNames {
 
     public static void main(String [] args) {
-        var names =  List.of("Susan","John","Michel", "Mitchell");
+       // var names =  List.of("Susan","John","Michel", "Mitchell");
+        List<String> names = new ArrayList<>(Arrays.asList("Susan","John","Michel", "Mitchell"));
 
         // Following using a lambda
         names.forEach(name -> System.out.println(name));
@@ -11,5 +18,29 @@ public class PrintNames {
         // Following using a method references
         names.forEach(System.out::println);
 
+        // We have 4 types of method references
+
+        // Method references to a static method
+        names.sort(PrintNames::compareByLength);
+
+        // Method references to an instance method for a specific object
+        names.forEach(System.out::println);
+
+        // Method references to an instance method to be called for the first argument
+        Function<String, String> transformer = String::toUpperCase;
+        System.out.println(transformer.apply("Susan"));
+
+        // Method reference to a constructor
+        Supplier<List<String>> listFactory = ArrayList::new;
+        var list = listFactory.get();
+        list.add("Shrirang");
+        list.add("Mahesh");
+        list.forEach(System.out::println);
+
     }
+
+    static int compareByLength(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+
 }
